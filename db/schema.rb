f577891722d8246243fdbd01c3b4e8b340a09746
Syuji <commentonly.is@gmail.com>
2015-10-13 08:15:20 +0900

    ExecJSがエラーを吐く問題に対処した。 http://qiita.com/scivola/items/ec7625118fcf6de5203a

--- db/schema.rb
+++ db/schema.rb
@@ -53,10 +53,11 @@ ActiveRecord::Schema.define(version: 20151012142027) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name",                                null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
