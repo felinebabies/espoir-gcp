80e1be5541beeea208fe713d3e50b8bf37e15337
Syuji <commentonly.is@gmail.com>
2015-10-12 21:14:52 +0900

    ゲームモデルを編集可能にした。また、トップに開催中のゲーム情報を掲載した。

--- app/controllers/entrance_controller.rb
+++ app/controllers/entrance_controller.rb
@@ -1,4 +1,5 @@
 class EntranceController < ApplicationController
   def index
+    @games = Game.all
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   enum status: { preparation: 0, session: 1, liquidation: 2, finish: 3 }
+
 end
